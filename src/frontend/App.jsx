75982c9cb378276cf889fa8870e27a047bdd3edd
DamnbronotJohnwick <businessbusiness1506@gmail.com>
2026-05-07 13:05:53 -0300

    Add App.jsx entry point

---
+++ src/frontend/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AgentGrid from "./components/AgentGrid";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#0A0E27]">
+      <AgentGrid />
+    </div>
+  );
+}
+
+export default App;
